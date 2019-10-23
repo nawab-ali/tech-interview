05cfc47784a804b5a0873e7a0349bc5b1a7546fe
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-10-23 01:22:12 +0000

    1. Implement is_matrix_empty() function 2. Add unit tests

--- OpenMP/matmul/include/matmul.h
+++ OpenMP/matmul/include/matmul.h
@@ -7,4 +7,44 @@
 #ifndef MATMUL_H
 #define MATMUL_H
 
+#include<vector>
+#include<stdexcept>
+using namespace std;
+
+typedef vector<vector<double>> matrix_t;
+
+/**
+ * @brief Is matrix m empty?
+ * @param m Matrix m
+ * @return True if m is empty
+ */
+bool is_matrix_empty(matrix_t& m) {
+	if (!m.size()) {
+		return(true);
+	} else if (!m[0].size()) {
+		return(true);
+	} else {
+		return(false);
+	}
+}
+
+/**
+ * @brief Multiply 2 matrices serially
+ * @param a Matrix A
+ * @param b Matrix B
+ * @return Matrix C = A x B
+ */
+matrix_t serial_matmul(matrix_t& a, matrix_t& b) {
+	long a_rows = 0;
+	long b_rows = 0;
+	long a_cols = 0;
+	long b_cols = 0;
+
+	if (is_matrix_empty(a) || is_matrix_empty(b)) {
+		throw invalid_argument("Empty A or B matrix");
+	}
+
+	return(a);
+}
+
 #endif //MATMUL_H
--- OpenMP/matmul/src/unit_tests.cpp
+++ OpenMP/matmul/src/unit_tests.cpp
@@ -8,8 +8,27 @@
 #include<gtest/gtest.h>
 
 // Unit tests for matrix multipication
-TEST (MatMulTest, Dummy) {
-	ASSERT_EQ(0, 0);
+TEST (MatMulTest, TestForEmptyMatrixEmptyRows) {
+	matrix_t m;
+	ASSERT_EQ(is_matrix_empty(m), true);
+}
+
+TEST (MatMulTest, TestForEmptyMatrixEmptyColumns) {
+	matrix_t a;
+	vector<double> b;
+
+	a.push_back(b);
+	ASSERT_EQ(is_matrix_empty(a), true);
+}
+
+TEST (MatMulTest, TestForEmptyMatrixFalse) {
+	matrix_t a;
+	vector<double> b;
+
+	b.push_back(3.0);
+	a.push_back(b);
+
+	ASSERT_EQ(is_matrix_empty(a), false);
 }
 
 int main(int argc, char** argv) {

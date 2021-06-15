1aa39df4e1fb17ac00b5f187574de88aa98a6eb2
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-06-15 07:33:01 -0700

    Reformat code.

--- C++/misc/data_structures.cpp
+++ C++/misc/data_structures.cpp
@@ -8,84 +8,84 @@
 using namespace std;
 
 void vector_tests(void) {
-  vector<int> v1, v3;
-  vector<double> v2(10);
-  
-  for (int i = 0; i < 10; i++) {
-    v1.push_back(i);
-    cout << v1[i] << " ";
-  }
-  cout << endl;
-  cout << v1.size() << endl;
-  
-  v3 = v1;
-  
-  for (int i = 0; i < 10; i++) {
-    v1.pop_back();
+    vector<int> v1, v3;
+    vector<double> v2(10);
+
+    for (int i = 0; i < 10; i++) {
+        v1.push_back(i);
+        cout << v1[i] << " ";
+    }
+    cout << endl;
     cout << v1.size() << endl;
-  }
-  
-  v3.erase(v3.begin()+2);
-  v3.insert(v3.begin()+2, 17);
-  for (const auto& n : v3) {
-    cout << n << " ";
-  }
-  cout << endl;
+
+    v3 = v1;
+
+    for (int i = 0; i < 10; i++) {
+        v1.pop_back();
+        cout << v1.size() << endl;
+    }
+
+    v3.erase(v3.begin() + 2);
+    v3.insert(v3.begin() + 2, 17);
+    for (const auto &n : v3) {
+        cout << n << " ";
+    }
+    cout << endl;
 }
 
 void stack_tests(void) {
-  stack<int> s;
-  
-  for (int i = 0; i < 10; i++) {
-    s.push(i);
-  }
-  
-  while (!s.empty()) {
-    cout << s.top() << " ";
-    s.pop();
-  }
-  cout << endl;
+    stack<int> s;
+
+    for (int i = 0; i < 10; i++) {
+        s.push(i);
+    }
+
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
 }
 
 void queue_tests(void) {
-  queue<int> q;
-  
-  for (int i = 0; i < 10; i++) {
-    q.push(i);
-  }
-  
-  while (!q.empty()) {
-    cout << q.front() << " ";
-    q.pop();
-  }
-  cout << endl;
+    queue<int> q;
+
+    for (int i = 0; i < 10; i++) {
+        q.push(i);
+    }
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
 }
 
 void umap_tests(void) {
-  unordered_map<string, double> umap;
-  
-  umap["pi"] = 3.14;
-  umap["root2"] = 1.414;
-  umap["root3"] = 1.732;
-  umap["log10"] = 2.302;
-  umap["loge"] = 1.0;
-  
-  for (const auto& p : umap) {
-    cout << p.first << ":" << p.second << endl;
-  }
-  
-  umap.erase("log10");
-  
-  for (const auto& p : umap) {
-    cout << p.first << ":" << p.second << endl;
-  }
+    unordered_map<string, double> umap;
+
+    umap["pi"] = 3.14;
+    umap["root2"] = 1.414;
+    umap["root3"] = 1.732;
+    umap["log10"] = 2.302;
+    umap["loge"] = 1.0;
+
+    for (const auto &p : umap) {
+        cout << p.first << ":" << p.second << endl;
+    }
+
+    umap.erase("log10");
+
+    for (const auto &p : umap) {
+        cout << p.first << ":" << p.second << endl;
+    }
 }
 
 int main(int argc, char **argv) {
-  //vector_tests();
-  //stack_tests();
-  //queue_tests();
-  umap_tests();
-  
-  return 0;
+    //vector_tests();
+    //stack_tests();
+    //queue_tests();
+    umap_tests();
+
+    return 0;
 }
--- C++/misc/inheritance.cpp
+++ C++/misc/inheritance.cpp
@@ -5,78 +5,85 @@
 using namespace std;
 
 class Shape {
-  public:
+public:
     virtual double area(void) = 0;
     virtual ~Shape() {}
 };
 
 class Rectangle : public Shape {
-  public:
-    Rectangle(const double length=0.0, const double width=0.0) : length(length), width(width) {}
+public:
+    Rectangle(const double length = 0.0, const double width = 0.0) : length(length), width(width) {}
+
     double area(void) {
-      return (length * width);
+        return (length * width);
     }
+
     ~Rectangle() {}
-  
-  protected:
+
+protected:
     double length;
     double width;
 };
 
 class Square : public Rectangle {
-  public:
+public:
     Square(const double length) : Rectangle(length, length) {}
     ~Square() {}
 };
 
 class PaintJob {
-  public:
+public:
     PaintJob(const double cost_per_sqfeet = 0.0) : cost_per_sqfeet(cost_per_sqfeet) {}
+
     double cost(const double area) const {
-      return (area * cost_per_sqfeet);
+        return (area * cost_per_sqfeet);
     }
+
     void set_cost_per_sqfeet(const double cost) {
-      cost_per_sqfeet = cost;
+        cost_per_sqfeet = cost;
     }
+
     virtual ~PaintJob() {}
-  
-  protected:
+
+protected:
     double cost_per_sqfeet;
 };
 
 class Circle : public Shape, public PaintJob {
-  public:
+public:
     Circle(const double radius = 0.0) : radius(radius) {}
+
     double area(void) {
-      const double PI = 3.14;
-      
-      return (PI * pow(radius, 2));
+        const double PI = 3.14;
+
+        return (PI * pow(radius, 2));
     }
+
     ~Circle() {}
-  
-  private:
+
+private:
     double radius;
 };
 
 double calc_area(Shape *ptr) {
-  return (ptr->area());
+    return (ptr->area());
 }
 
 int main(int argc, char **argv) {
-  Circle *cptr = new Circle(9.5);
-  Square *sptr = new Square(8.0);
-  Rectangle *rptr = new Rectangle(7.9, 2.4);
-  
-  cout << "Circle area: " << calc_area(cptr) << endl;
-  cout << "Square area: " << calc_area(sptr) << endl;
-  cout << "Rectangle area: " << calc_area(rptr) << endl;
-  
-  cptr->set_cost_per_sqfeet(10.7);
-  cout << "Circle paint cost: " << cptr->cost(cptr->area()) << endl;
-  
-  delete cptr;
-  delete sptr;
-  delete rptr;
-  
-  return 0;
+    Circle *cptr = new Circle(9.5);
+    Square *sptr = new Square(8.0);
+    Rectangle *rptr = new Rectangle(7.9, 2.4);
+
+    cout << "Circle area: " << calc_area(cptr) << endl;
+    cout << "Square area: " << calc_area(sptr) << endl;
+    cout << "Rectangle area: " << calc_area(rptr) << endl;
+
+    cptr->set_cost_per_sqfeet(10.7);
+    cout << "Circle paint cost: " << cptr->cost(cptr->area()) << endl;
+
+    delete cptr;
+    delete sptr;
+    delete rptr;
+
+    return 0;
 }

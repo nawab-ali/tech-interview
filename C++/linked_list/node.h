84fa76c37faeaba518fdba92060d700037d7a1e6
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-06-21 11:27:57 -0700

    Reformat code.

--- C++/linked_list/node.h
+++ C++/linked_list/node.h
@@ -3,38 +3,38 @@
 #ifndef NODE_H
 #define NODE_H
 
-template <class T>
+template<class T>
 class Node {
-	private:
-		T data;
-		Node<T>* next;
+private:
+    T data;
+    Node<T> *next;
 
-	public:
-		Node(const T& data) : data(data), next(nullptr) {}
-		T get_data(void) const;
-		Node<T>* get_next(void) const;
-		void set_data(const T& data);
-		void set_next(Node<T>* const next);
+public:
+    Node(const T &data) : data(data), next(nullptr) {}
+    T get_data(void) const;
+    Node<T> *get_next(void) const;
+    void set_data(const T &data);
+    void set_next(Node<T> *const next);
 };
 
-template <class T>
+template<class T>
 T Node<T>::get_data(void) const {
-	return(data);
+    return (data);
 }
 
-template <class T>
-Node<T>* Node<T>::get_next(void) const {
-	return(next);
+template<class T>
+Node<T> *Node<T>::get_next(void) const {
+    return (next);
 }
 
-template <class T>
-void Node<T>::set_data(const T& data) {
-	this->data = data;
+template<class T>
+void Node<T>::set_data(const T &data) {
+    this->data = data;
 }
 
-template <class T>
-void Node<T>::set_next(Node<T>* const next) {
-	this->next = next;
+template<class T>
+void Node<T>::set_next(Node<T> *const next) {
+    this->next = next;
 }
 
 #endif //NODE_H
